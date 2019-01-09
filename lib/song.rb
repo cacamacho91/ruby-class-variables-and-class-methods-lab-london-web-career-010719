f06af66ad992333f1919c687e55ab56e273ff7ba
Chris Camacho <cacamacho91@gmail.com>
2019-01-09 18:03:07 +0000

    done da lab

---
+++ lib/song.rb
@@ -0,0 +1,58 @@
+require 'pry'
+
+class Song
+  attr_accessor :name
+  attr_accessor :artist
+  attr_accessor :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist ,genre )
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    count = {}
+    @@genres.each do |genre|
+      if count.keys.include?(genre)
+        count[genre] += 1
+      else
+        count[genre] = 1
+      end
+    end
+    count
+  end
+
+  def self.artist_count
+    count = {}
+    @@artists.each do |artist|
+      if count.keys.include?(artist)
+        count[artist] += 1
+      else
+        count[artist] = 1
+      end
+    end
+    count
+  end
+
+end
